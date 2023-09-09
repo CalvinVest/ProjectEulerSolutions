599190954a4b2de446a5fdcb7d2c80a85ab5d075
Calvin Vest <calvinjvest@gmail.com>
2023-09-09 09:18:03 -0500

    Adds documentation to JCL

--- src/projecteulersolutions/JavaClassLoader.java
+++ src/projecteulersolutions/JavaClassLoader.java
@@ -19,19 +19,24 @@ based on user input.
 public class JavaClassLoader extends ClassLoader {
     public void invokeClassMethod(String className, String methodName) {
         try {
+            // new ClassLoader for invocation
             ClassLoader cLoader = this.getClass().getClassLoader();
+            // loads class of given class name
             Class myClass = cLoader.loadClass(className); // can throw cnfe
-            
+            // uses class constructor to instantiate new object of given class
             Constructor constructor = myClass.getConstructor(); // can throw nsme
             Object myClassObject = constructor.newInstance(); // can throw ie, iae, ite
-            
+            // uses class to get the proper given method
             Method myMethod = myClass.getMethod(methodName);
+            // uses instatiated class object to invoke the given method
             myMethod.invoke(myClassObject);
-        } catch (ClassNotFoundException
-                | NoSuchMethodException
-                | InstantiationException
-                | IllegalAccessException
-                | InvocationTargetException e) {
+        } catch (ClassNotFoundException // from .loadClass();
+                | NoSuchMethodException // from .getMethod(); and .getConstructor();
+                | InstantiationException // from .newInstance();
+                | IllegalAccessException // from .newInstance(); and .invoke(Object);
+                | InvocationTargetException e) { // from .newInstance(); and .invoke(Object);
+            // basic exception printing
+            // this should be expanded to more granular solutions in the future
             System.out.println("Exception encountered - " + e);   
         }
     }
