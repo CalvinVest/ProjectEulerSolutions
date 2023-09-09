a09cfa2f7672c61f4241a4b08c65bfa7b8303d54
Calvin Vest <calvinjvest@gmail.com>
2023-09-09 10:10:45 -0500

    Adds invokeProblemByNumber base functionality

--- src/projecteulersolutions/ProjectEulerSolutions.java
+++ src/projecteulersolutions/ProjectEulerSolutions.java
@@ -6,4 +6,11 @@ public class ProjectEulerSolutions {
 
     }
     
+    public static void invokeProblemByNumber(int problemNumber) {
+        String problemNumberText = String.format("%04d", problemNumber);
+        JavaClassLoader jcl = new JavaClassLoader();
+        
+        System.out.println("Problem " + problemNumber + ": ");
+        jcl.invokeClassMethod("projecteulersolutions.Problem" + problemNumberText, "printAnswer");
+    }
 }
