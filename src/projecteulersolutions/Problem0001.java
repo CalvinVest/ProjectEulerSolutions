b9ec3cdee04a4f46ab908b781fdf9c31bf4dad42
Calvin Vest <calvinjvest@gmail.com>
2023-09-09 12:34:45 -0500

    Adds problem0001 functionality and basic documentation

--- src/projecteulersolutions/Problem0001.java
+++ src/projecteulersolutions/Problem0001.java
@@ -1,8 +1,16 @@
 package projecteulersolutions;
 
 public class Problem0001 extends Problem {
+    /*
+    The goal of problem 1 is to find the sum of all
+    natural numbers divisible by 3 or 5 below 1000
+    */
     @Override
     public void printAnswer() {
-        
+        int sum = 0;
+        for(int i = 1; i < 1000; i++)
+            if(i % 3 == 0 || i % 5 == 0)
+                sum += i;
+        System.out.println("The sum of all numbers divisible by 3 or 5 below 1000 is: " + sum);
     }
 }
