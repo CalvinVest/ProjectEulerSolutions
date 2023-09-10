554a44e9b121fc5818dc67a8a23271325c5b1516
Calvin Vest <calvinjvest@gmail.com>
2023-09-09 19:55:00 -0500

    Adds documentation to problem0001

--- src/projecteulersolutions/Problem0001.java
+++ src/projecteulersolutions/Problem0001.java
@@ -7,10 +7,10 @@ public class Problem0001 extends Problem {
     */
     @Override
     public void printAnswer() {
-        int sum = 0;
-        for(int i = 1; i < 1000; i++)
-            if(i % 3 == 0 || i % 5 == 0)
-                sum += i;
+        int sum = 0; // answer variable, sum of all qualifying values
+        for(int i = 1; i < 1000; i++) // all nat numbers below 1000
+            if(i % 3 == 0 || i % 5 == 0) // any multiples of 3 or 5 qualify
+                sum += i; // adds qualifying values to the sum
         System.out.println("The sum of all numbers divisible by 3 or 5 below 1000 is: " + sum);
     }
 }
