7097a98e351b7850d78fe215e017e4fb01a2d9a5
Calvin Vest <calvinjvest@gmail.com>
2023-09-11 14:21:55 -0500

    Adds problem0003 class and problem description

---
+++ src/projecteulersolutions/Problem0003.java
@@ -0,0 +1,12 @@
+package projecteulersolutions;
+
+public class Problem0003 extends Problem {
+    /*
+    The goal of problem 3 is to find the
+    largest prime factor of 600851475143.
+    */
+    @Override
+    public void printAnswer() {
+        
+    }
+}
