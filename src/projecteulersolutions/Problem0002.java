02410eaa3dcda853fdc6db7efa2c9f0194ae1f45
Calvin Vest <calvinjvest@gmail.com>
2023-09-11 14:16:59 -0500

    Addl documentation and algorithm correction

--- src/projecteulersolutions/Problem0002.java
+++ src/projecteulersolutions/Problem0002.java
@@ -1,20 +1,28 @@
 package projecteulersolutions;
 
 public class Problem0002 extends Problem {
+    /*
+    The goal of problem 2 is to find the sum of all
+    even-numbered Fibonacci numbers under four million.
+    */
     @Override
     public void printAnswer() {
-        int curr = 0;
-        int last1 = 1;
-        int last2 = 1;
-        long sum = 0;
+        int curr = 0; // current fibonacci number
+        int last1 = 1; // first fibonacci prior number
+        int last2 = 1; // second fibonacci prior number
+        long sum = 0; // running sum of even fibonacci numbers
+        
         
         while (curr < 4000000) {
+            // calculate current fibonacci number
             curr = last1 + last2;
-            if(curr % 2 == 0)
-                sum += curr;
+            if(curr % 2 == 0) // even check
+                sum += curr; // adds current fibonacci number to sum
+            
+            // cycle prior fibonacci numbers
+            // to be added at start of next loop
             last2 = last1;
             last1 = curr;
-            curr = last1 + last2;
         }
         
         System.out.println("The sum of all even Fibonacci numbers below 4,000,000 is " + sum);
