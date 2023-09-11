f6ce63c9839d67af36de4d3e627913652dbe5998
Calvin Vest <calvinjvest@gmail.com>
2023-09-11 15:59:10 -0500

    Problem 4 class and description

---
+++ src/projecteulersolutions/Problem0004.java
@@ -0,0 +1,12 @@
+package projecteulersolutions;
+
+/*
+The goal of problem 4 is to find the largest palindromic
+product of two three-digit numbers.
+
+Find max(x)
+Where x is y * z, 100 <= y < 1000, 100 <= z < 1000
+*/
+public class Problem0004 extends Problem {
+    
+}
