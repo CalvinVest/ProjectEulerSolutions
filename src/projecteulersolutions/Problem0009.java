ffeba32d5afd35a0718ec05f49d9c4070038bd7f
Calvin Vest <calvinjvest@gmail.com>
2023-09-13 00:09:43 -0500

    Adds Problem 9 class and problem goal

---
+++ src/projecteulersolutions/Problem0009.java
@@ -0,0 +1,13 @@
+package projecteulersolutions;
+
+/*
+The goal of Problem 9 is to find the product of the values of the
+only existing Pythagorean triplet whose three values sum to 1000.
+
+That is,
+a^2 + b^2 = c^2, a + b + c = 1000, a < b < c
+find a*b*c
+*/
+public class Problem0009 extends Problem {
+    
+}
