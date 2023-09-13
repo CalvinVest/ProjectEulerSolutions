37c26cc9de92832724598475f038c10191cd8ca8
Calvin Vest <calvinjvest@gmail.com>
2023-09-12 22:52:16 -0500

    Adds problem 6 functionality

---
+++ src/projecteulersolutions/Problem0006.java
@@ -0,0 +1,33 @@
+package projecteulersolutions;
+
+/*
+The goal of problem 6 is to find the difference between
+the sum of the squares of the first 100 natural numbers and
+the square of the sum of the first 100 natural numbers.
+
+Essentially,
+(1 + 2 + ... + 100)^2 - (1^2 + 2^2 + ... + 100^2)
+
+When it came to finding the size of the necessary variables, the 
+largest number we'll be dealing with is the square of the sum of the
+first 100 natural numbers. The total value would be 5050^2 which is
+well within the bounds of int.
+*/
+public class Problem0006 extends Problem {
+    
+    @Override
+    public void printAnswer() {
+        int sum = 0;
+        for(int i = 1; i <= 100; i++) {
+            sum += i;
+        }
+        int squareOfSums = sum * sum;
+        int sumOfSquares = 0;
+        for(int i = 1; i <= 100; i++) {
+            sumOfSquares += i * i;
+        }
+        
+        System.out.println("The difference between the sum of squares and square of sums for 1-100 is "
+            + (squareOfSums - sumOfSquares));
+    }
+}
