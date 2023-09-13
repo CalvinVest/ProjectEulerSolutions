bc0346f7f6913a821eb633f49e0348b81d9fc3ad
Calvin Vest <calvinjvest@gmail.com>
2023-09-12 23:18:48 -0500

    Adds isPrime functionality and fixed problem 7 algorithm

--- src/projecteulersolutions/Problem0007.java
+++ src/projecteulersolutions/Problem0007.java
@@ -10,7 +10,7 @@ public class Problem0007 extends Problem {
         int countPrimes = 0;
         int latestPrime = 0;
         
-        for(int i = 1; countPrimes < 10001; i++) {
+        for(int i = 2; countPrimes < 10001; i++) {
             if (isPrime(i)) {
                 countPrimes++;
                 latestPrime = i;
@@ -18,5 +18,12 @@ public class Problem0007 extends Problem {
         }
         
         System.out.println("The 10001st prime number is " + latestPrime);
+    }
+    
+    private boolean isPrime(int n) {
+        for(int i = 2; i <= n / 2; i++) {
+            if (n % i == 0) return false;
+        }
+        return true;
     }
 }
