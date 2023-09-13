a5c534e38c87b92fb3571d90904210b3de7541df
Calvin Vest <calvinjvest@gmail.com>
2023-09-12 23:14:11 -0500

    Adds problem 7 functionality without isPrime implmt

--- src/projecteulersolutions/Problem0007.java
+++ src/projecteulersolutions/Problem0007.java
@@ -5,4 +5,18 @@ The goal of problem 7 is to find the 10001st prime number
 */
 public class Problem0007 extends Problem {
     
+    @Override
+    public void printAnswer() {
+        int countPrimes = 0;
+        int latestPrime = 0;
+        
+        for(int i = 1; countPrimes < 10001; i++) {
+            if (isPrime(i)) {
+                countPrimes++;
+                latestPrime = i;
+            }
+        }
+        
+        System.out.println("The 10001st prime number is " + latestPrime);
+    }
 }
