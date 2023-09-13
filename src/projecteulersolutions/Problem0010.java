fa6d2abc772260698094308c08c033e33e362d5c
Calvin Vest <calvinjvest@gmail.com>
2023-09-13 09:28:21 -0500

    Adds problem 10 functionality with runtime three minutes

--- src/projecteulersolutions/Problem0010.java
+++ src/projecteulersolutions/Problem0010.java
@@ -4,6 +4,29 @@ package projecteulersolutions;
 The goal of problem 10 is to find the sum of
 all primes below two million
 */
-public class Problem0010 {
+public class Problem0010 extends Problem {
     
+    @Override
+    public void printAnswer() {
+        long sum = 2l;
+        
+        for(int i = 3; i < 2000000; i+=2) {
+            if(isPrime(i)){
+                sum += i;
+                System.out.println(i + " is prime");
+            }
+            
+        }
+        
+        System.out.println("The sum of all primes below two million is " + sum);
+    }
+    
+    
+    
+    private boolean isPrime(int n) {
+        for(int i = 2; i <= n / 2; i++) {
+            if (n % i == 0) return false;
+        }
+        return true;
+    }
 }
