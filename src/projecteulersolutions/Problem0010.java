02fb27be98f4262bfb87bd4fb1ec0b1e72583537
Calvin Vest <calvinjvest@gmail.com>
2023-09-13 12:21:10 -0500

    Adds sieve of eratosthenes algorithm to speed up problem 10

--- src/projecteulersolutions/Problem0010.java
+++ src/projecteulersolutions/Problem0010.java
@@ -3,30 +3,42 @@ package projecteulersolutions;
 /*
 The goal of problem 10 is to find the sum of
 all primes below two million
+
+The Sieve of Eratosthenes is the most efficient way to find
+all primes smaller than a given value for values below about
+ten million.
 */
 public class Problem0010 extends Problem {
     
     @Override
     public void printAnswer() {
-        long sum = 2l;
+        long sum = sumSieveOfEratosthenes(sieveOfEratosthenes(2000000));
         
-        for(int i = 3; i < 2000000; i+=2) {
-            if(isPrime(i)){
-                sum += i;
-                System.out.println(i + " is prime");
-            }
-            
-        }
         
         System.out.println("The sum of all primes below two million is " + sum);
     }
     
-    
-    
-    private boolean isPrime(int n) {
-        for(int i = 2; i <= n / 2; i++) {
-            if (n % i == 0) return false;
+    private boolean[] sieveOfEratosthenes(int n) {
+        boolean[] truthArray = new boolean[n + 1];
+        for(int i = 0; i <= n; i++) {
+            truthArray[i] = true;
         }
-        return true;
+        
+        for(int i = 2; i * i <= n; i++) {
+            if(truthArray[i]) {
+                for(int j = i * i; j < n; j += i)
+                    truthArray[j] = false;
+            }
+        }
+        return truthArray;
+    }
+    
+    private long sumSieveOfEratosthenes(boolean[] primes) {
+        long sum = 0l;
+        for(int i = 2; i < primes.length; i++) {
+            if(primes[i])
+                sum += i;
+        }
+        return sum;
     }
 }
