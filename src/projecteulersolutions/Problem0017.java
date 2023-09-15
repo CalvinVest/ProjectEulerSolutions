8c95f5cb9a910a230df3895046dbe941edf2e539
Calvin Vest <calvinjvest@gmail.com>
2023-09-15 07:53:29 -0500

    Adds problem 17 documentation

---
+++ src/projecteulersolutions/Problem0017.java
@@ -0,0 +1,16 @@
+package projecteulersolutions;
+
+/*
+The goal of problem 17 is to find the sum of all characters used to write
+the numbers 1 to 1000 in words.
+
+f(5) = "one" + "two" + "three" + "four" + "five" = 19
+find f(1000).
+ */
+public class Problem0017 extends Problem {
+
+    @Override
+    public void printSolution() {
+
+    }
+}
