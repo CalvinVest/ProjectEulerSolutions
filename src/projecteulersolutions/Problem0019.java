a9f173e69b686c98f3f1073ef15a7322e8273493
Calvin Vest <calvinjvest@gmail.com>
2023-09-16 08:55:07 -0500

    Adds problem 19 no functionality

---
+++ src/projecteulersolutions/Problem0019.java
@@ -0,0 +1,13 @@
+package projecteulersolutions;
+
+/*
+The goal of problem 19 is to find the number of Sundays that fall on the 
+first of a month during the twentieth century. (1 Jan 1901 to 31 Dec 2000)
+ */
+public class Problem0019 extends Problem {
+
+    @Override
+    public void printSolution() {
+
+    }
+}
