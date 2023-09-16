cf96753e5bbb6312eeeb2ac539d5e711be355379
Calvin Vest <calvinjvest@gmail.com>
2023-09-16 08:46:06 -0500

    Adds problem 18 and text file no functionality

---
+++ src/projecteulersolutions/Problem0018.java
@@ -0,0 +1,12 @@
+package projecteulersolutions;
+
+/*
+The goal of problem 18 is to find the maximum path traversal through the 
+tree given in problem0018.txt
+*/
+public class Problem0018 extends Problem {
+    
+    @Override public void printSolution() {
+        
+    }
+}
