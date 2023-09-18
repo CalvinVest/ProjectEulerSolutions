27b9103452d5a15bc0f05e6a71e7586a2ccac299
Calvin Vest <calvinjvest@gmail.com>
2023-09-18 08:52:27 -0500

    Adds problem 20 class no functionality

---
+++ src/projecteulersolutions/Problem0020.java
@@ -0,0 +1,16 @@
+package projecteulersolutions;
+
+/*
+The goal of problem 20 is to find the sum of the digits of (100)!
+
+(10)! = 10 * 9 * ... * 1 = 3628800
+f(10) = 3 + 6 + 2 + 8 + 8 + 0 + 0 = 27
+find f(100)
+*/
+public class Problem0020 extends Problem {
+    
+    @Override
+    public void printSolution() {
+        
+    }
+}
