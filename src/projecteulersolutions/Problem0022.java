6ee0d835a761c06f693ec6468c2581d5736708f8
Calvin Vest <calvinjvest@gmail.com>
2023-09-18 11:15:21 -0500

    Adds problem 22 name trimming

--- src/projecteulersolutions/Problem0022.java
+++ src/projecteulersolutions/Problem0022.java
@@ -16,7 +16,7 @@ The score of a name is its alphanumeric sum times its place in the alphabetized 
     For example, Colin is 938th in the list, so Colin's score is 53 * 938
 
 Plan:
-Import list
+Import list - done
 Alphabetize list
 Output list for debug
 Implement alphanumeric sum function
@@ -31,7 +31,17 @@ public class Problem0022 extends Problem {
         File namesFile = new File(Problem.FILEPATH + "problem0022.txt");
         
         List<String> names = readNamesFromFile(namesFile);
+        List<String> trimmedNames = trimNames(names);
         
+        trimmedNames.forEach(name -> System.out.println(name));
+    }
+    
+    private List<String> trimNames(List<String> names) {
+        List<String> newNames = new ArrayList<>();
+        
+        names.forEach(name -> newNames.add(name.substring(1, name.length() - 1)));
+        
+        return newNames;
     }
 
     private List<String> readNamesFromFile(File file) {
@@ -45,7 +55,6 @@ public class Problem0022 extends Problem {
         } catch(FileNotFoundException fnfe) {
             System.out.println("Exception encountered: " + file.toString() + " does not exist.");
         }
-        System.out.println("All names loaded from file");
         return names;
     }
 }
