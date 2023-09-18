24e732cf07f1dbcc1773a9146286c2deaa03fada
Calvin Vest <calvinjvest@gmail.com>
2023-09-18 11:07:38 -0500

    Adds problem 22 read file to string array

--- src/projecteulersolutions/Problem0022.java
+++ src/projecteulersolutions/Problem0022.java
@@ -1,5 +1,11 @@
 package projecteulersolutions;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 /*
 The goal of problem 22 is to find the sum of all name scores for the given file
 
@@ -17,11 +23,29 @@ Implement alphanumeric sum function
 Implement name score function
 Sum name scores
 Print solution
-*/
+ */
 public class Problem0022 extends Problem {
-    
+
     @Override
     public void printSolution() {
+        File namesFile = new File(Problem.FILEPATH + "problem0022.txt");
         
+        List<String> names = readNamesFromFile(namesFile);
+        
+    }
+
+    private List<String> readNamesFromFile(File file) {
+        ArrayList<String> names = new ArrayList<>();
+        try {
+            Scanner fileIn = new Scanner(file);
+            fileIn.useDelimiter(",");
+            while(fileIn.hasNext()) {
+                names.add(fileIn.next());
+            }
+        } catch(FileNotFoundException fnfe) {
+            System.out.println("Exception encountered: " + file.toString() + " does not exist.");
+        }
+        System.out.println("All names loaded from file");
+        return names;
     }
 }
