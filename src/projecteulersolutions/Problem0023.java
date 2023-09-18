275ec05cb038b5a02f90437a1538855996808917
Calvin Vest <calvinjvest@gmail.com>
2023-09-18 12:57:09 -0500

    Adds problem 23 class no functionality and goal

---
+++ src/projecteulersolutions/Problem0023.java
@@ -0,0 +1,20 @@
+package projecteulersolutions;
+
+/*
+The goal of problem 23 is to find the sum of all positive integers which cannot
+be expressed as the sum of two abundant numbers.
+
+A perfect number is one where the sum of its factors is equal to the number.
+A deficient number is one where the sum of factors is less than the number.
+An abundant number is one where the sum of factors is more than the number.
+
+By mathematical analysis, all numbers > 28123 can be written as the sum of two
+abundant numbers.
+ */
+public class Problem0023 extends Problem {
+
+    @Override
+    public void printSolution() {
+
+    }
+}
