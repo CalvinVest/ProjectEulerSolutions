d479c01680d89f99017f6939a5d653a273224585
Calvin Vest <calvinjvest@gmail.com>
2023-09-18 09:32:20 -0500

    Adds problem 20 documentation

--- src/projecteulersolutions/Problem0020.java
+++ src/projecteulersolutions/Problem0020.java
@@ -13,14 +13,18 @@ public class Problem0020 extends Problem {
 
     @Override
     public void printSolution() {
+        // BigInteger object to hold the value of 100!
         BigInteger factorial = getFactorial(100);
+        // int value to hold the sum of the digits of 100!
         int digitSum = sumDigits(factorial);
         System.out.println("The sum of the digits of (100)! is " + digitSum);
     }
 
+    // getFactorial(int n) returns the factorial value n! as a BigInteger
     private BigInteger getFactorial(int n) {
         BigInteger factorial = BigInteger.ONE;
 
+        // for all values i = 2 to n, multiply factorial by i
         for (int i = 2; i <= n; i++) {
             factorial = factorial.multiply(BigInteger.valueOf(i));
         }
@@ -29,11 +33,15 @@ public class Problem0020 extends Problem {
     }
 
     private int sumDigits(BigInteger n) {
+        // int sum to hold sum of digit values
         int sum = 0;
 
+        // splits the given BigInteger into one-char strings of each digit
         String[] numStrs = n.toString().split("");
 
+        // for each digit of the given BigInteger value
         for (String str : numStrs) {
+            // add the digit value to the sum of digit values
             sum += Integer.parseInt(str);
         }
 
