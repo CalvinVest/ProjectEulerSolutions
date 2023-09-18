9faff8efd34271fba466d0813ba28659109c2f12
Calvin Vest <calvinjvest@gmail.com>
2023-09-18 10:14:52 -0500

    Adds problem 22 class no functionality with goal and plan

---
+++ src/projecteulersolutions/Problem0022.java
@@ -0,0 +1,27 @@
+package projecteulersolutions;
+
+/*
+The goal of problem 22 is to find the sum of all name scores for the given file
+
+The file provided is a list of over 5,000 names.
+The alphanumeric sum of a number is the sum of its characters a=1 values.
+    For example, Colin's alphanumeric sum  is c=3 + o=15 + l=12 + i=9 + n=14 = 53
+The score of a name is its alphanumeric sum times its place in the alphabetized list
+    For example, Colin is 938th in the list, so Colin's score is 53 * 938
+
+Plan:
+Import list
+Alphabetize list
+Output list for debug
+Implement alphanumeric sum function
+Implement name score function
+Sum name scores
+Print solution
+*/
+public class Problem0022 extends Problem {
+    
+    @Override
+    public void printSolution() {
+        
+    }
+}
