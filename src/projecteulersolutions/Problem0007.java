d9eda128874d0ea62266f1d41f17118fe34660dc
Calvin Vest <calvinjvest@gmail.com>
2023-09-19 00:05:12 -0500

    Adds isSolved to and reformats problems 6 to 10

--- src/projecteulersolutions/Problem0007.java
+++ src/projecteulersolutions/Problem0007.java
@@ -2,27 +2,34 @@ package projecteulersolutions;
 
 /*
 The goal of problem 7 is to find the 10001st prime number
-*/
+ */
 public class Problem0007 extends Problem {
-    
+
+    @Override
+    public boolean isSolved() {
+        return true;
+    }
+
     @Override
     public void printSolution() {
         int countPrimes = 0;
         int latestPrime = 0;
-        
-        for(int i = 2; countPrimes < 10001; i++) {
+
+        for (int i = 2; countPrimes < 10001; i++) {
             if (isPrime(i)) {
                 countPrimes++;
                 latestPrime = i;
             }
         }
-        
+
         System.out.println("The 10001st prime number is " + latestPrime);
     }
-    
+
     private boolean isPrime(int n) {
-        for(int i = 2; i <= n / 2; i++) {
-            if (n % i == 0) return false;
+        for (int i = 2; i <= n / 2; i++) {
+            if (n % i == 0) {
+                return false;
+            }
         }
         return true;
     }
--- src/projecteulersolutions/Problem0010.java
+++ src/projecteulersolutions/Problem0010.java
@@ -7,37 +7,43 @@ all primes below two million
 The Sieve of Eratosthenes is the most efficient way to find
 all primes smaller than a given value for values below about
 ten million.
-*/
+ */
 public class Problem0010 extends Problem {
-    
+
+    @Override
+    public boolean isSolved() {
+        return true;
+    }
+
     @Override
     public void printSolution() {
         long sum = sumSieveOfEratosthenes(sieveOfEratosthenes(2000000));
-        
-        
+
         System.out.println("The sum of all primes below two million is " + sum);
     }
-    
+
     private boolean[] sieveOfEratosthenes(int n) {
         boolean[] truthArray = new boolean[n + 1];
-        for(int i = 0; i <= n; i++) {
+        for (int i = 0; i <= n; i++) {
             truthArray[i] = true;
         }
-        
-        for(int i = 2; i * i <= n; i++) {
-            if(truthArray[i]) {
-                for(int j = i * i; j < n; j += i)
+
+        for (int i = 2; i * i <= n; i++) {
+            if (truthArray[i]) {
+                for (int j = i * i; j < n; j += i) {
                     truthArray[j] = false;
+                }
             }
         }
         return truthArray;
     }
-    
+
     private long sumSieveOfEratosthenes(boolean[] primes) {
         long sum = 0l;
-        for(int i = 2; i < primes.length; i++) {
-            if(primes[i])
+        for (int i = 2; i < primes.length; i++) {
+            if (primes[i]) {
                 sum += i;
+            }
         }
         return sum;
     }
