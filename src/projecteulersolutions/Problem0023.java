cc327c7ac801968adc6dcfa92b0f9c232c084cb9
Calvin Vest <calvinjvest@gmail.com>
2023-09-19 00:18:23 -0500

    Adds isSolved to problems 21 to 23

--- src/projecteulersolutions/Problem0023.java
+++ src/projecteulersolutions/Problem0023.java
@@ -16,69 +16,74 @@ abundant numbers.
 public class Problem0023 extends Problem {
 
     @Override
+    public boolean isSolved() {
+        return true;
+    }
+
+    @Override
     public void printSolution() {
         System.out.println("The sum of all non-abundant sums is " + abundantSum(28123));
     }
-    
+
     private int abundantSum(int limit) {
         int[] array = new int[limit + 1];
-        
+
         ArrayList<Integer> list = abundantList(limit);
-        
-        for(int i = 0; i < list.size(); i++) {
-            for(int j = i; j < list.size(); j++) {
+
+        for (int i = 0; i < list.size(); i++) {
+            for (int j = i; j < list.size(); j++) {
                 int sum = list.get(i) + list.get(j);
-                if(sum <= limit) {
+                if (sum <= limit) {
                     array[sum] = sum;
                 }
             }
         }
-        
+
         int sum = 0;
-        for(int i = 1; i < limit; i++) {
-            if(array[i] != i) {
+        for (int i = 1; i < limit; i++) {
+            if (array[i] != i) {
                 sum += i;
             }
         }
         return sum;
     }
-    
+
     private ArrayList<Integer> abundantList(int limit) {
         ArrayList<Integer> list = new ArrayList<>();
-        
-        for(int i = 12; i <= limit; i++) {
-            if(isAbundant(i)) {
+
+        for (int i = 12; i <= limit; i++) {
+            if (isAbundant(i)) {
                 list.add(i);
             }
         }
         return list;
     }
-    
+
     private boolean isAbundant(int n) {
         int sqrt = (int) Math.sqrt(n);
         var list = new ArrayList<Integer>();
         int sum = 0;
-        
+
         list.add(1);
-        for(int i = 2; i <= sqrt; i++) {
-            if(n % i == 0) {
+        for (int i = 2; i <= sqrt; i++) {
+            if (n % i == 0) {
                 list.add(i);
-                list.add(n/i);
+                list.add(n / i);
             }
         }
         list = removeDuplicates(list);
-        
-        for(int i : list) {
+
+        for (int i : list) {
             sum += i;
         }
         return sum > n;
     }
-    
+
     private ArrayList<Integer> removeDuplicates(ArrayList<Integer> list) {
         ArrayList<Integer> newList = new ArrayList<>();
-        
-        for(int i : list) {
-            if(!newList.contains(i)) {
+
+        for (int i : list) {
+            if (!newList.contains(i)) {
                 newList.add(i);
             }
         }
