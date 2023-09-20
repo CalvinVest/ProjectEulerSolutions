384e596c72bd7621452fe7a360658758063db83d
Calvin Vest <calvinjvest@gmail.com>
2023-09-20 08:08:12 -0500

    Adds problem 48, 49, and 92 classes, no functionality

---
+++ src/projecteulersolutions/Problem0049.java
@@ -0,0 +1,14 @@
+package projecteulersolutions;
+
+public class Problem0049 extends Problem {
+
+    @Override
+    public boolean isSolved() {
+        return false;
+    }
+
+    @Override
+    public void printSolution() {
+        System.out.println("This solution has not been found yet.");
+    }
+}
