e2f1f7369ec37ac4d0bc65d3ef38a2fe941b9763
Calvin Vest <calvinjvest@gmail.com>
2023-09-20 23:19:12 -0500

    Adds problem 461 with goal no functionality

---
+++ src/projecteulersolutions/Problem0461.java
@@ -0,0 +1,21 @@
+package projecteulersolutions;
+
+/*
+Let fn(k) = e^(k/n) - 1 for k is Z > 0
+We are given f200(6) + f200(75) + f200(89) + f200(226)
+ = 3.141592644529 or almost pi.
+It is the best approximation of pi for n = 200
+with the expression fn(a) + fn(b) + fn(c) + fn(d)
+*/
+public class Problem0461 extends Problem {
+    
+    @Override
+    public boolean isSolved() {
+        return false;
+    }
+    
+    @Override
+    public void printSolution() {
+        
+    }
+}
