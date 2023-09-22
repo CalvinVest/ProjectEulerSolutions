5112470cf738229628f49be4b6b344d0d152b47d
Calvin Vest <calvinjvest@gmail.com>
2023-09-22 18:23:42 -0500

    Adds problem 26 class with goal no functionality

---
+++ src/projecteulersolutions/Problem0026.java
@@ -0,0 +1,34 @@
+package projecteulersolutions;
+
+/*
+The goal of problem 26 is to find the number < 1000 for which
+1/d contains the longest recurring cycle in its decimal fraction
+part.
+
+A unit fraction contains 1 in the numerator. The decimal representation
+of the unit fractions with denominators 2 to 10 are:
+1/2 = 0.5
+1/3 = 0.(3)
+1/4 = 0.25
+1/5 = 0.2
+1/6 = 0.1(6)
+1/7 = 0.(142857)
+1/8 = 0.125
+1/9 = 0.(1)
+1/10 = 0.1
+
+1/6 has a 1-digit recurring cycle, the repeating 6.
+1/7 has a 6-digit recurring cycle, the repeating 142857.
+*/
+public class Problem0026 extends Problem {
+    
+    @Override
+    public boolean isSolved() {
+        return false;
+    }
+    
+    @Override
+    public void printSolution() {
+        
+    }
+}
