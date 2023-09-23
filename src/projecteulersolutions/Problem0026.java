eaf3a6b65a33aae037a2134f07cfc449f4b890f7
Calvin Vest <calvinjvest@gmail.com>
2023-09-22 19:46:33 -0500

    Adds problem 26 functionality with getcyclelength

--- src/projecteulersolutions/Problem0026.java
+++ src/projecteulersolutions/Problem0026.java
@@ -1,5 +1,8 @@
 package projecteulersolutions;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /*
 The goal of problem 26 is to find the number < 1000 for which
 1/d contains the longest recurring cycle in its decimal fraction
@@ -19,16 +22,41 @@ of the unit fractions with denominators 2 to 10 are:
 
 1/6 has a 1-digit recurring cycle, the repeating 6.
 1/7 has a 6-digit recurring cycle, the repeating 142857.
-*/
+ */
 public class Problem0026 extends Problem {
-    
+
     @Override
     public boolean isSolved() {
         return false;
     }
-    
+
     @Override
     public void printSolution() {
-        
+        int maxLength = 0;
+        int num = 0;
+
+        for (int i = 2; i < 1000; i++) {
+            int length = getCycleLength(i);
+            if (length > maxLength) {
+                maxLength = length;
+                num = i;
+            }
+        }
+
+        System.out.println("The number less than 1000 with the longest recurring cycle is " + num);
+    }
+
+    private static int getCycleLength(int n) {
+
+        Map<Integer, Integer> digitLength = new HashMap<>();
+        int digit = 1; // holds value of next digit
+        int length = 0; // length of recurring cycle
+        while (!digitLength.containsKey(digit)) {
+            digitLength.put(digit, length);
+            digit = digit * 10 % n;
+            length++;
+        }
+        //stateToIter.values().forEach(key -> System.out.println(key));
+        return length - digitLength.get(digit);
     }
 }
