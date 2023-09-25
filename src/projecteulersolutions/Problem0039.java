249efc6bea56a0554427ab2ccf307a551e85d153
Calvin Vest <calvinjvest@gmail.com>
2023-09-25 09:55:28 -0500

    Adds problem 39 goal

--- src/projecteulersolutions/Problem0039.java
+++ src/projecteulersolutions/Problem0039.java
@@ -1,6 +1,11 @@
 package projecteulersolutions;
 
-
+/*
+If p is the perimeter of a right angle triangle with integral length
+sides, {a, b, c}, there are exactly three solutions for p = 120.
+{20, 48, 52}, {24, 45, 51}, {30, 40, 50}
+Find which value for p <= 1000 the number of solutions is maximised.
+*/
 public class Problem0039 extends Problem {
     
     @Override
