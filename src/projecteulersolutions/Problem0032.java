0aba6856d5e6eb3bd23fd5e2ec6e60c5d47a20c4
Calvin Vest GwentPC <calvinjvest@gmail.com>
2023-09-24 22:06:04 -0500

    Adds problem 32 problem description

--- src/projecteulersolutions/Problem0032.java
+++ src/projecteulersolutions/Problem0032.java
@@ -1,12 +1,25 @@
 package projecteulersolutions;
 
+/*
+The goal of problem 32 is to find the sum of all products whose
+multiplicand/multiplier/product can be written as a 1 through 9
+pandigital.
+
+We shall say that an n-digit number is pandigital if it makes 
+use of all the digits 1 to n exactly once; for example, the
+5-digit number, 15234 is 1 through 5 pandigital.
+
+The product 7254 is unusual, as the identity 39 * 186 = 7254,
+containing multiplicand, multiplier, and product is 1 through 9
+pandigital.
+ */
 public class Problem0032 extends Problem {
-    
+
     @Override
     public boolean isSolved() {
         return false;
     }
-    
+
     @Override
     public void printSolution() {
         System.out.println("This problem has not been solved.");
