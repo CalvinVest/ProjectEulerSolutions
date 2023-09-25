955b12b96ecd75985ca2626e8b723d540a568943
Calvin Vest GwentPC <calvinjvest@gmail.com>
2023-09-24 20:31:07 -0500

    Adds recursive solution for problem 24

--- src/projecteulersolutions/Problem0024.java
+++ src/projecteulersolutions/Problem0024.java
@@ -4,9 +4,11 @@ package projecteulersolutions;
 The goal of problem 24 is to find the millionth lexicographic permutation of
 the digits 0, 1, 2, 3, 4, 5, 6, 7, 8, and 9.
 
-p(1) = 0123456789
-p(2) = 0123456798
-p(3) = 0123456879
+p(0) = 0123456789
+p(1) = 0123456798
+p(2) = 0123456879
+
+find p(999999) (the millionth permutation)
  */
 public class Problem0024 extends Problem {
 
@@ -17,26 +19,48 @@ public class Problem0024 extends Problem {
 
     @Override
     public void printSolution() {
-        String solution = getNthPermutation("0123456789".toCharArray(), 1000000);
-        System.out.println("The millionth permutation of 0123456789 is: " + solution);
+        try {
+            String solution = getNthPermutationRecursive("0123456789".toCharArray(), 999999);
+            System.out.println("The millionth permutation of 0123456789 is: " + solution);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
     }
 
-    private String getNthPermutation(char[] str, int n) {
-        int length = str.length;
-        StringBuilder result = new StringBuilder();
+    private String getNthPermutationRecursive(char[] str, int n) {
+        if (str.length == 1) {
+            // the last remaining char is returned to break recursion
+            return Character.toString(str[0]);
+        }
+        int factorial = getFactorial(str.length - 1);
+        // the number of times the current factorial divides into the given n
+        int dividend = n / factorial;
+        // the remainder from the above division
+        int remainder = n % factorial;
+        // char at the index for how many times the current factorial goes into n
+        char currentChar = str[dividend];
 
-        for (int i = 0; i < length; i++) {
-            int index = n / (length - i);
-            result.append(str[index]);
-            // Remove the used character from the array
-            for (int j = index; j < length - i - 1; j++) {
-                str[j] = str[j + 1];
+        // adds all characters that are not the current char to the new char array
+        char[] newStr = new char[str.length - 1];
+        for (int i = 0; i < str.length; i++) {
+            if (i < dividend) {
+                // have not yet encountered the current char index
+                newStr[i] = str[i];
+            } else if (i > dividend) {
+                // have encountered current char index, adjust index offset
+                newStr[i - 1] = str[i];
             }
-            n %= (length - i);
         }
 
-        return result.toString();
+        // recursively adds all characters to the return string
+        // in correct order for given permutation count
+        return currentChar + getNthPermutationRecursive(newStr, remainder);
     }
 
-  
+    private int getFactorial(int n) {
+        if (n <= 1) {
+            return 1;
+        }
+        return n * getFactorial(n - 1);
+    }
 }
