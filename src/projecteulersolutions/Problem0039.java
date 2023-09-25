1db0954e56c61753f4084deabe810ee29a615bc8
Calvin Vest <calvinjvest@gmail.com>
2023-09-25 09:52:59 -0500

    Adds problem 39 class no functionality

---
+++ src/projecteulersolutions/Problem0039.java
@@ -0,0 +1,15 @@
+package projecteulersolutions;
+
+
+public class Problem0039 extends Problem {
+    
+    @Override
+    public boolean isSolved() {
+        return false;
+    }
+    
+    @Override
+    public void printSolution() {
+        System.out.println("This problem has not been solved.");
+    }
+}
