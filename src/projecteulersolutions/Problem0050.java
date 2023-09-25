72d155db508128455b7d36ee27360b52bc37382a
Calvin Vest <calvinjvest@gmail.com>
2023-09-25 12:39:22 -0500

    Adds problem 50 goal

--- src/projecteulersolutions/Problem0050.java
+++ src/projecteulersolutions/Problem0050.java
@@ -1,5 +1,14 @@
 package projecteulersolutions;
 
+/*
+The goal of problem 50 is to find the prime below one million that can be written
+as the sum of the most consecutive primes.
+
+The prime 41 can be written as the sum of six consecutive primes:
+41 = 2 + 3 + 5 + 7 + 11 + 13
+This is the longest sum of consecutive primes that adds to a prime below 100.
+The longest sum of consecutive primes below 1000 has 21 terms and is 953.
+*/
 public class Problem0050 extends Problem {
     
     @Override
