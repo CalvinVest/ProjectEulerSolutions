91823c2aa81a850160f8fcc792d8db0a33a52161
Calvin Vest GwentPC <calvinjvest@gmail.com>
2023-09-24 23:32:31 -0500

    Adds problem 41 class with goal no functionality

---
+++ src/projecteulersolutions/Problem0041.java
@@ -0,0 +1,20 @@
+package projecteulersolutions;
+
+/*
+The goal of problem 41 is to find the largest n-digit pandigital prime
+that exists. An n-digit pandigital number makes use of all the digits
+1 to n exactly once.
+For example, 2143 is a 4-digit pandigital number.
+ */
+public class Problem0041 extends Problem {
+
+    @Override
+    public boolean isSolved() {
+        return false;
+    }
+
+    @Override
+    public void printSolution() {
+
+    }
+}
