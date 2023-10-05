ab022a6a05272fe23c766b9107f06423017bcbf4
Calvin Vest GwentPC <calvinjvest@gmail.com>
2023-10-05 13:30:23 -0500

    Rebuilds project, bugfixes broken code from problem 41 merge, updates readme to reflect current progress

--- src/projecteulersolutions/Problem0041.java
+++ src/projecteulersolutions/Problem0041.java
@@ -1,7 +1,5 @@
 package projecteulersolutions;
 
-import java.util.ArrayList;
-import java.util.Collections;
 
 /*
 The goal of problem 41 is to find the largest n-digit pandigital prime
@@ -136,7 +134,7 @@ public class Problem0041 extends Problem {
 
     /*
     containsDigit(int[], int) returns if the given array contains the given int
-    */
+     */
     private boolean containsDigit(int[] arr, int d) {
         for (int i : arr) {
             // if the given int matches any value of the array return true
@@ -146,51 +144,5 @@ public class Problem0041 extends Problem {
         }
         // array does not contain value, return false
         return false;
-        int maxPandigitalPrime = 0;
-        /* 9-digit pandigitals have a digital sum of
-        1+2+3+4+5+6+7+8+9=45. A digital sum divisible by 3 indicates
-        that the number is divisible by three
-        
-        This means the upper bound is now max 8-digit pandigital.
-        1+2+3+4+5+6+7+8=36. A digital sum divisible by 3 indicates
-        that the number is divisible by three
-        
-        This means the upper bound is now max 7-digit pandigital.
-        
-        It can be readily assumed that there exists a 7-digit pandigital
-        prime, so lower bound can be min 7-digit pandigital.
-         */
-        for (int i = 7654321; i >= 1234567; i -= 2) {
-            if (isPandigital(i) && isPrime(i)) {
-                maxPandigitalPrime = i;
-                break;
-            }
-        }
-        System.out.println("The largest pandigital prime is " + maxPandigitalPrime);
-    }
-
-    private boolean isPandigital(int n) {
-        String str = "" + n;
-        ArrayList<Character> charList = new ArrayList<>();
-        for (char c : str.toCharArray()) {
-            charList.add(c);
-        }
-        Collections.sort(charList);
-        String newStr = "";
-        String matchStr = "";
-        for (int i = 0; i < charList.size(); i++) {
-            newStr += charList.get(i).toString();
-            matchStr += i + 1;
-        }
-        return newStr.matches(matchStr);
-    }
-
-    private boolean isPrime(int n) {
-        for (int i = 2; i <= n / 2; i++) {
-            if (n % i == 0) {
-                return false;
-            }
-        }
-        return true;
     }
 }
