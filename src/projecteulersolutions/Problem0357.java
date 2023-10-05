de3a721bb4454f88ad3bce204337e95e7f215733
Calvin Vest GwentPC <calvinjvest@gmail.com>
2023-10-05 14:04:00 -0500

    Adds problem 41 isDivisor and isPrime methods

--- src/projecteulersolutions/Problem0357.java
+++ src/projecteulersolutions/Problem0357.java
@@ -8,16 +8,29 @@ It can be seen that for every divisor d of 30, d + 30/d is prime.
 
 Find the sum of all positive integers n not exceeding 100,000,000
 such that for every divisor d of n, d + n/d is prime.
-*/
+ */
 public class Problem0357 extends Problem {
-    
+
     @Override
     public boolean isSolved() {
         return false;
     }
-    
+
     @Override
     public void printSolution() {
         System.out.println("This solution has not been implemented.");
+    }
+
+    private boolean isDivisor(int d, int n) {
+        return n % d == 0;
+    }
+
+    private boolean isPrime(int n) {
+        for (int i = 2; i <= n / 2; i++) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+        return true;
     }
 }
