b21324e1819e8762a794d6bc40b54bb21b2fc043
Calvin Vest GwentPC <calvinjvest@gmail.com>
2023-10-05 15:20:50 -0500

    Adds problem 357 functionality not quite working, active development

--- src/projecteulersolutions/Problem0357.java
+++ src/projecteulersolutions/Problem0357.java
@@ -18,11 +18,48 @@ public class Problem0357 extends Problem {
 
     @Override
     public void printSolution() {
-        System.out.println("This solution has not been implemented.");
+        long sum = 0l;
+
+        for (int i = 4; i <= 100000000; i++) {
+            if (i % 100000 == 0) {
+                System.out.println(i + " =======================================");
+            }
+            if (!isPrime(i) && hasAllPrimeCofactorSums(i)) {
+                sum += i;
+            }
+        }
+
+        System.out.println("The sum of all numbers with all prime cofactor sums below 100000000 is " + sum);
+        // need to loop through all values
+        // for(int i = 1; i <= 100000000; i++) {
+
+        // for each number that may be a factor within given value
+        // for(int j = 2; j <= (int) Math.sqrt(i); j++) {
+        // is it a divisor?
+        // if(i % j == 0) {
+        // if so is the divisor plus cofactor prime?
+        // if(isPrime(j + i/j)
+        // if it isn't then this number isn't valid, break
+        // if(!isPrime(j + i/j)) {
+        // if it is then continue through the loop for each divisor
+        // go through all divisors for the number, then do it again
     }
 
-    private boolean isDivisor(int d, int n) {
-        return n % d == 0;
+    /*
+    hasAllPrimeCofactorSums(int) returns if a given ints divisors are all prime
+    such that for every divisor d of n, d + n/d is prime.
+     */
+    private boolean hasAllPrimeCofactorSums(int n) {
+        for (int i = 2; i <= n / 2; i++) {
+
+            if (n % i == 0) {
+                if (!isPrime(i + n / i)) {
+                    return false;
+                }
+            }
+        }
+        //System.out.println(n + " has all prime cofactor sums.");
+        return true;
     }
 
     private boolean isPrime(int n) {
