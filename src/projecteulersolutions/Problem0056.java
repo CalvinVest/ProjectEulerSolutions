15ce6810b2464a51f3886dd36ded8eb985874fdb
Calvin Vest GwentPC <calvinjvest@gmail.com>
2023-10-06 06:49:23 -0500

    Adds problem 56 goal no functionality

--- src/projecteulersolutions/Problem0056.java
+++ src/projecteulersolutions/Problem0056.java
@@ -1,5 +1,14 @@
 package projecteulersolutions;
 
+/*
+The goal of problem 56:
+
+A googol (10^100) is a massive number: one followed by one-hundred zeros; 10^100
+is almost unimaginably large: one followed by two-hundred zeros. Despite their size,
+the sum of the digits in each number is only 1.
+
+Considering natural numbers of the for a^b where a,b < 100, what is the maximum digital sum?
+*/
 public class Problem0056 extends Problem {
     
     @Override
