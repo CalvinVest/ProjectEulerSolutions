053c9852f78fecfab02008f36edba683877f0e3e
Calvin Vest GwentPC <calvinjvest@gmail.com>
2023-10-06 07:09:30 -0500

    Revises problem 56 optimization with with practical base 10 case pruning

--- src/projecteulersolutions/Problem0056.java
+++ src/projecteulersolutions/Problem0056.java
@@ -10,20 +10,23 @@ is almost unimaginably large: one followed by two-hundred zeros. Despite their s
 the sum of the digits in each number is only 1.
 
 Considering natural numbers of the for a^b where a,b < 100, what is the maximum digital sum?
-*/
+ */
 public class Problem0056 extends Problem {
-    
+
     @Override
     public boolean isSolved() {
         return false;
     }
-    
+
     @Override
     public void printSolution() {
         int maxSum = 0;
-        
-        for(int i = 2; i < 100; i++) {
-            for(int j = 1; j < 100; j++) {
+
+        for (int i = 2; i < 100; i++) {
+            for (int j = 2; j < 100; j++) {
+                if (i % 10 == 0 || j % 10 == 0) {
+                    continue;
+                }
                 BigInteger num = getBigIntPow(i, j);
                 int digitSum = getBigIntDigitSum(num);
                 if (digitSum > maxSum) {
@@ -33,16 +36,16 @@ public class Problem0056 extends Problem {
         }
         System.out.println("The largest digital sum of two two-digit powers is " + maxSum);
     }
-    
+
     private BigInteger getBigIntPow(int a, int b) {
         BigInteger bigA = new BigInteger(Integer.toString(a));
         return bigA.pow(b);
     }
-    
+
     private int getBigIntDigitSum(BigInteger n) {
         int sum = 0;
         int curr = 0;
-        while(!"0".equals(n.toString())) {
+        while (!"0".equals(n.toString())) {
             curr = n.mod(BigInteger.TEN).intValue();
             sum += curr;
             n = n.divide(BigInteger.TEN);
