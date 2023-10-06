08d7e574755812837584fd31838bbc9657a438af
Calvin Vest GwentPC <calvinjvest@gmail.com>
2023-10-06 06:24:49 -0500

    Adds problem 357 documentation

--- src/projecteulersolutions/Problem0357.java
+++ src/projecteulersolutions/Problem0357.java
@@ -22,9 +22,12 @@ public class Problem0357 extends Problem {
 
     @Override
     public void printSolution() {
+        // sieve is a sieve of eratosthenes with a range from 0 to our LIMIT value, 100,000,000
         sieve = sieveOfEratosthenes(LIMIT + 1);
+        // sum variable to hold sum of all numbers whose factors are all prime cofactor sums
         long sum = 0l;
 
+        // for every value within the given range
         for (int n = 0; n <= LIMIT; n++) {
             if (sieve[n + 1] && isPrimeCofactorSum(n)) {
                 sum += n;
@@ -49,15 +52,18 @@ public class Problem0357 extends Problem {
         if (n < 0) {
             throw new IllegalArgumentException("Negative array size");
         }
+        // make a bool array for values 0 to n
         boolean[] sieve = new boolean[n + 1];
+        // if array is 2 or more, 2 is prime
         if (n >= 2) {
             sieve[2] = true;
         }
+        // for all odd values, set to true
         for (int i = 3; i <= n; i += 2) {
             sieve[i] = true;
         }
-
-        for (int i = 3, end = (int) Math.sqrt(n); i <= end; i += 2) {
+        // for all odd values up to sqrt n, set to false if not prime
+        for (int i = 3; i <= (int) Math.sqrt(n); i += 2) {
             if (sieve[i]) {
                 int increment = 2 * i;
                 for (int j = i * i; j <= n; j += increment) {
