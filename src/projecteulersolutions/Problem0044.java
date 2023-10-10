62c14d3646ed2865b8a0ebc131342f38955a5ca7
Calvin Vest GwentPC <calvinjvest@gmail.com>
2023-10-10 08:46:07 -0500

    Adds problems 27, 37, 44, no functionality, updates readme

---
+++ src/projecteulersolutions/Problem0044.java
@@ -0,0 +1,14 @@
+package projecteulersolutions;
+
+public class Problem0044 extends Problem {
+    
+    @Override
+    public boolean isSolved() {
+        return false;
+    }
+    
+    @Override
+    public void printSolution() {
+        
+    }
+}
