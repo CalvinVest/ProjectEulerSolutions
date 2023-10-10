68e17d0441bdd00e18e8d01b76948d990f6cab44
Calvin Vest GwentPC <calvinjvest@gmail.com>
2023-10-10 13:20:47 -0500

    Adds problem 100 goal no functionality

--- src/projecteulersolutions/Problem0100.java
+++ src/projecteulersolutions/Problem0100.java
@@ -1,5 +1,17 @@
 package projecteulersolutions;
 
+/*
+If a box contains twenty-one colored discs, composes of fifteen blue discs and six
+red discs, and two discs were taken at random, it can be seen that the probability
+of taking two blue discs,
+P(BB) = (15/21) * (14/20) = 1/2
+
+The next such arrangement, for which there is exactly 50% chance of taking two blue
+discs at random, is a box containing 85 blue discs and 35 red discs.
+
+By finding the arrangement to contain over 10^12 = 1000000000000 discs in total,
+determine the number of blue discs that the box would contain.
+*/
 public class Problem0100 extends Problem {
     
     @Override
@@ -9,6 +21,6 @@ public class Problem0100 extends Problem {
     
     @Override
     public void printSolution() {
-        System.out.println("This problem has not been solved yet.");
+        
     }
 }
