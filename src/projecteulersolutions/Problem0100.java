58f0f70ed575f2879d92c7b2bdc0c18a7e4a58aa
Calvin Vest GwentPC <calvinjvest@gmail.com>
2023-11-23 12:47:43 -0600

    Revises problem 100 algorithm to use blue count for outer for loop

--- src/projecteulersolutions/Problem0100.java
+++ src/projecteulersolutions/Problem0100.java
@@ -11,16 +11,30 @@ discs at random, is a box containing 85 blue discs and 35 red discs.
 
 By finding the arrangement to contain over 10^12 = 1000000000000 discs in total,
 determine the number of blue discs that the box would contain.
-*/
+ */
 public class Problem0100 extends Problem {
-    
+
+    private static final double MIN_T = 1E12;
+
     @Override
     public boolean isSolved() {
         return false;
     }
-    
+
     @Override
     public void printSolution() {
-        
+
+        for (double b = MIN_T / 2; b < MIN_T; b++) {
+            for (double r = 0; r < b; r++) {
+                double t = r + b;
+                if (isPerfectPbb(b, t)) {
+                    System.out.println("The number of blue discs with a perfect 50% chance of being drawn twice is " + b);
+                }
+            }
+        }
+    }
+
+    private boolean isPerfectPbb(double b, double t) {
+        return b * (b - 1) / t / (t - 1) == 0.5;
     }
 }
