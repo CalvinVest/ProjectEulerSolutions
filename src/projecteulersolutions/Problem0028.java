ea661b3718c576c17f3de76618fb2eeb5e3ad6bc
Calvin Vest <calvinjvest@gmail.com>
2023-11-26 12:07:01 -0600

    Adds problem 28 doc

--- src/projecteulersolutions/Problem0028.java
+++ src/projecteulersolutions/Problem0028.java
@@ -11,18 +11,21 @@ public class Problem0028 extends Problem {
     
     @Override
     public void printSolution() {
+        // 1 is the center of any spiral, so it is auto added to the sum
         long sum = 1l;
+        // incrementer starts at 2 and increases by 2 every rotation, or 4 cycles
         int inc = 2;
+        // clock to hold number of cycles within a given rotation. [0, 3]
         int clock = 0;
         
-        for(int i = 3; i <= MAX * MAX; i += inc) {
+        // range is any value in spiral matrix of size MAX*MAX
+        // only diagonal values of the matrix are considered and added to sum
+        for(int i = 1 + inc; i <= MAX * MAX; i += inc) {
             sum += i;
-            System.out.println("Added " + i + ", sum is " + sum);
             clock++;
             if(clock == 4) {
                 inc += 2;
                 clock = 0;
-                System.out.println("Clock is reset. Incrementer is now " + inc);
             }
         }
         System.out.println("The sum of the number spiral diagonals for a " + MAX + "x" + MAX + " spiral is " + sum);
