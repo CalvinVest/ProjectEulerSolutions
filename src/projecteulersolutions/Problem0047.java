386b0af9d514b828b536a635e8716b4132b97c65
Calvin Vest <calvinjvest@gmail.com>
2023-11-27 11:00:04 -0600

    Adds problem 47 functionality, 50s runtime

--- src/projecteulersolutions/Problem0047.java
+++ src/projecteulersolutions/Problem0047.java
@@ -1,5 +1,8 @@
 package projecteulersolutions;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /*
 The first two consecutive numbers to have two distinct prime factors are:
 14 = 2 x 7
@@ -15,6 +18,8 @@ What is the first of these numbers?
  */
 public class Problem0047 extends Problem {
 
+    public static final int CONSECUTIVE_COUNT = 4;
+
     @Override
     public boolean isSolved() {
         return false;
@@ -22,6 +27,39 @@ public class Problem0047 extends Problem {
 
     @Override
     public void printSolution() {
+        int sol = 0;
+        boolean solutionFound = false;
+        for (int n = 2; !solutionFound; n++) {
+            if (isConsecutiveDistinctFactors(n, CONSECUTIVE_COUNT)) {
+                sol = n;
+                solutionFound = true;
+            }
+        }
 
+        System.out.println("The first four consecutive integers with four distinct primes each starts at " + sol);
+    }
+
+    private int getCountOfDistinctPrimeFactors(int n) {
+        int count = 0;
+        int prime = 2;
+        while (n > 1) {
+            if (n % prime == 0) {
+                count++;
+                while (n % prime == 0) {
+                    n /= prime;
+                }
+            }
+            prime = EulerMath.getNextPrime(prime);
+        }
+        return count;
+    }
+
+    private boolean isConsecutiveDistinctFactors(int n, int range) {
+        for (int i = 0; i < range; i++) {
+            if (getCountOfDistinctPrimeFactors(n + i) < range) {
+                return false;
+            }
+        }
+        return true;
     }
 }
