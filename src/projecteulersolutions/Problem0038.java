40ca9cc54ba926b3707836c13093f4023d9466b0
Calvin Vest <calvinjvest@gmail.com>
2023-11-26 21:06:12 -0600

    Revises problem 38 upper bounds and adds documentation

--- src/projecteulersolutions/Problem0038.java
+++ src/projecteulersolutions/Problem0038.java
@@ -24,28 +24,42 @@ public class Problem0038 extends Problem {
 
     @Override
     public void printSolution() {
-        
+
+        // largest pandigital number made from concatenated products
         int largestPan = 0;
-        
-        for(int i = 1; i < 1000000; i++) {
-            for(int j = 2; j <= 9; j++) {
+
+        // arbitrary upper bound of one million, simply because the smallest
+        // possible i value for a product array of {1,2} would be 
+        for (int i = 9; i < 100000; i++) {
+            // for arrays {1,2} to {1,2,3,4,5} since a 5 member array is the largest
+            // that can product an answer, 9 x {1,2,3,4,5}.
+            for (int j = 2; j < 5; j++) {
+                // concatenated string, e.g. "9" + "18" + "27" + "36" + "45"
+                // for i = 9, j = 5, concat str = 918273645
                 String concatStr = getConcatProdStr(i, j);
-                if(concatStr.length() != 9) {
+                // if the string isnt 9 digits long, it's not a 1-9 pandigital
+                if (concatStr.length() != 9) {
                     continue;
                 }
+                // if the found number is pandigital and larger than the greatest
+                // found pandigital number, replace it.
                 int concatNum = Integer.parseInt(concatStr);
-                if(EulerMath.isPandigital(concatNum) && concatNum > largestPan) {
+                if (EulerMath.isPandigital(concatNum) && concatNum > largestPan) {
                     largestPan = concatNum;
                 }
             }
         }
-        
+
         System.out.println("The largest concatenated pandigital number is " + largestPan);
     }
-    
+
+    // for given inputs n and iter, returns the concatenation of all multiples of n
+    // up to n * iter, for example getConcatProdStr(9, 5) returns "9" + "18" + "27"
+    // + "36" + "45" = "918273645".
     private String getConcatProdStr(int n, int iter) {
         String concatStr = "";
-        for(int i = 1; i <= iter; i++) {
+        // for every value 1 to iter, concatenate the result of n*iter to concatStr
+        for (int i = 1; i <= iter; i++) {
             concatStr += (i * n) + "";
         }
         return concatStr;
