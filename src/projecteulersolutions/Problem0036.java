677f9a2785199412039af04afc3bb846f9fa92d5
Calvin Vest <calvinjvest@gmail.com>
2023-11-26 20:27:10 -0600

    Adds problem 36 doc and removes debug prints

--- src/projecteulersolutions/Problem0036.java
+++ src/projecteulersolutions/Problem0036.java
@@ -10,6 +10,8 @@ Find the sum of all numbers, less than one million, which are palindromic in bas
  */
 public class Problem0036 extends Problem {
 
+    public static final int MAX = 1000000;
+    
     @Override
     public boolean isSolved() {
         return false;
@@ -17,14 +19,14 @@ public class Problem0036 extends Problem {
 
     @Override
     public void printSolution() {
-        long sum = 0l;
+        // holds sum of all base10/base2 palindromes
+        int sum = 0;
 
-        for (int i = 0; i < 1E6; i++) {
-            String binStr = Integer.toBinaryString(i);
-
-            if (EulerMath.isPalindrome(Integer.toString(i)) && EulerMath.isPalindrome(binStr)) {
+        // for every int below the max value
+        for (int i = 0; i < MAX; i++) {
+            // if base 10 is palindromic and base 2 is palindromic
+            if (EulerMath.isPalindrome(Integer.toString(i)) && EulerMath.isPalindrome(Integer.toBinaryString(i))) {
                 sum += i;
-                System.out.println(i + " (" + binStr + ") is palindromic in base 2 and base 10.");
             }
         }
 
