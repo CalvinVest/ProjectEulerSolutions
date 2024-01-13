462761f4b9852587876e2dbb885235fcc69aef16
Calvin Vest <calvinjvest@gmail.com>
2024-01-13 08:59:21 -0600

    Adds problem 71 goal

--- src/projecteulersolutions/Problem0071.java
+++ src/projecteulersolutions/Problem0071.java
@@ -1,5 +1,19 @@
 package projecteulersolutions;
 
+/*
+Consider the fraction, n/d, where n and d are positive integers. If n < d and
+HCF(n,d) = 1, it is called a reduced proper fraction.
+
+If we list the set of reduced proper fractions for d <= 8 in ascending order of
+size, we get:
+
+1/8, 1/7, 1/6, 1/5, 1/4, 2/7, 1/3, 3/8, 2/5, 3/7, 1/2, ...
+
+It can be seen that 2/5 is the fraction immediately to the left of 3/7.
+
+By listing the set of reduced proper fractions for d<= 1 000 000 in ascending
+order of size, find the numerator of the fraction immediately to the left of 3/7
+*/
 public class Problem0071 extends Problem {
 
     @Override
