1a3a1c4493ea3d9a9748a824e565970171c7aa99
Calvin Vest <calvinjvest@gmail.com>
2024-01-18 09:45:07 -0600

    Implements getExponentCeiling to problem 63 and removes debug printing

--- src/projecteulersolutions/Problem0063.java
+++ src/projecteulersolutions/Problem0063.java
@@ -7,7 +7,7 @@ The 5-digit number, 16807 = 7^5, is also a fifth power. Similarly, the 9-digit
 number, 134217728 = 8^9, is a ninth power.
 
 How many n-digit positive integers exist which are also an nth power?
-*/
+ */
 public class Problem0063 extends Problem {
 
     @Override
@@ -18,36 +18,36 @@ public class Problem0063 extends Problem {
     @Override
     public void printSolution() {
         System.out.println("This problem has not yet been solved.");
-        int powerfulDigitCount = getPowerfulDigitCount(100);
         
-        System.out.println(getExponentCeiling() + " is the max exponent.");
+        int powerfulDigitCount = getPowerfulDigitCount();
+
         System.out.println("The count of powerful digit numbers is " + powerfulDigitCount);
     }
-    
-    private int getPowerfulDigitCount(int arbitraryCeiling) {
+
+    private int getPowerfulDigitCount() {
         int count = 0;
-        for(int i = 1; i < 10; i++) { // a base >= 10 would result in more digits than the exponent value
-            for(int j = 1; j <= arbitraryCeiling; j++) {
+        int exponentCeiling = getExponentCeiling();
+        for (int i = 1; i < 10; i++) { // a base >= 10 would result in more digits than the exponent value
+            for (int j = 1; j <= exponentCeiling; j++) {
                 BigInteger bigIntPow = new BigInteger(Integer.toString(i)).pow(j);
                 String powString = bigIntPow.toString();
-                if(powString.length() == j) {
-                    System.out.println(powString + " is " + i + "^" + j + " and is " + powString.length() + " digits.");
+                if (powString.length() == j) {
                     count++;
                 }
             }
-            System.out.println(i);
         }
         return count;
     }
-    
+
     private int getExponentCeiling() {
         int ceiling = 1;
-        BigInteger nine = new BigInteger("9");
+
+        BigInteger nine = BigInteger.TEN.subtract(BigInteger.ONE);
         BigInteger pow = nine.pow(ceiling);
-        
+
         // eventually 9^x will be less than digits(x), giving the ceiling for
         // what exponents need actually be considered for getPowerfulDigitCount.
-        while(ceiling <= pow.toString().length()) {
+        while (ceiling <= pow.toString().length()) {
             ceiling++;
             pow = nine.pow(ceiling);
         }
