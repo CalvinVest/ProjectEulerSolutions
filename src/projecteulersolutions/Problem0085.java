17f5b72efa2d69ba0f80e10dfb8f7523433d0090
Calvin Vest <calvinjvest@gmail.com>
2024-03-10 16:19:19 -0500

    Adds problem 85 functionality

--- src/projecteulersolutions/Problem0085.java
+++ src/projecteulersolutions/Problem0085.java
@@ -9,6 +9,22 @@ public class Problem0085 extends Problem {
 
     @Override
     public void printSolution() {
-        System.out.println("This problem has not been solved.");
+        long area = 0;
+        long minError = 2000000l;
+
+        for (int h = 2; h < 1000; h++) {
+            for (int w = 2; w < 1000; w++) {
+                var currError = Math.abs(2000000 - getRectangles(h, w));
+                if (currError < minError) {
+                    minError = currError;
+                    area = h * w;
+                }
+            }
+        }
+        System.out.println("The area of a grid that can fit closest to two million rectangles is " + area);
+    }
+
+    private long getRectangles(int h, int w) {
+        return (h + 1) * h * (w + 1) * w / 4;
     }
 }
