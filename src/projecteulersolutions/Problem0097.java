743c1c9c3d5108bae39d52fbea427acb68b6c891
Calvin Vest <calvinjvest@gmail.com>
2024-03-10 12:26:47 -0500

    Adds problem 97 functionality

--- src/projecteulersolutions/Problem0097.java
+++ src/projecteulersolutions/Problem0097.java
@@ -1,5 +1,7 @@
 package projecteulersolutions;
 
+import java.math.BigInteger;
+
 /*
 The first known prime found to exceed one million digits was discovered in 1999,
 and is a Mersenne prime of the form 2^6972593 - 1; it comtains exactly 2098960
@@ -10,7 +12,7 @@ However, in 2004 there was found a massive non-Mersenne prime which contains
 2357207 digits: 28433 * 2^7830457 + 1.
 
 Find the last ten digits of this prime number.
-*/
+ */
 public class Problem0097 extends Problem {
 
     @Override
@@ -21,5 +23,25 @@ public class Problem0097 extends Problem {
     @Override
     public void printSolution() {
         System.out.println("This problem has not yet been solved.");
+        int[] powerArr = {1, 0, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1};
+
+        BigInteger tot = BigInteger.ONE;
+        BigInteger curr = BigInteger.TWO;
+
+        for (int i = 0; i < powerArr.length; i++) {
+            if (powerArr[i] == 1) {
+                System.out.println("Adding n = " + i + ", 2^2^n mod 1E10 = " + curr);
+                tot = tot.multiply(curr);
+                if (tot.toString().length() > 10) {
+                    tot = tot.remainder(BigInteger.TEN.pow(10));
+                }
+
+                System.out.println("tot = " + tot.toString());
+            }
+            curr = curr.pow(2).remainder(BigInteger.TEN.pow(10));
+        }
+        tot = tot.multiply(new BigInteger("28433")).add(BigInteger.ONE).remainder(BigInteger.TEN.pow(10));
+
+        System.out.println("tot = " + tot.toString());
     }
 }
