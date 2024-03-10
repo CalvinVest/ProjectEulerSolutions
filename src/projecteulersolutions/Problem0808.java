c0f9691a8b4af7b610977c801ac34b707ca501bf
Calvin Vest <calvinjvest@gmail.com>
2024-03-10 15:56:07 -0500

    Adds problem 808 nonworking functionality

--- src/projecteulersolutions/Problem0808.java
+++ src/projecteulersolutions/Problem0808.java
@@ -1,14 +1,56 @@
 package projecteulersolutions;
 
 public class Problem0808 extends Problem {
-    
+
     @Override
     public boolean isSolved() {
         return false;
     }
-    
+
     @Override
     public void printSolution() {
-        System.out.println("This problem has not been solved.");
+
+        int count = 0;
+        int i = 5;
+
+        while (count < 50) {
+            long currSquare = i * i;
+
+            if (!EulerMath.isPalindrome(Long.toString(currSquare))) {
+                long reverse = getReversedLong(currSquare);
+                if (isPerfectSquare(reverse)) {
+                    int reverseRoot = (int) Math.sqrt(reverse);
+                    if (EulerMath.isPrime(reverseRoot)) {
+                        count++;
+                        System.out.println("Reversible Prime Square #" + count + " found: " + currSquare);
+                    }
+                }
+            }
+
+            i = EulerMath.getNextPrime(i);
+        }
+    }
+
+    private long getReversedLong(long num) {
+        long rev = 0l;
+
+        while (num != 0) {
+            int d = (int) (num % 10);
+            rev = rev * 10 + d;
+            num /= 10;
+        }
+
+        return rev;
+    }
+
+    private boolean isPerfectSquare(long num) {
+        if (num < 0) {
+            return false; // Negative numbers are not perfect squares
+        }
+
+        int root = (int) Math.sqrt(num); // Integer square root
+        long checker = root * root;
+        // Check if square of root equals n
+        return checker == num;
     }
 }
