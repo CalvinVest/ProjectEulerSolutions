c73d25cf9b852a58b644f8197249c1abe2c2b309
Calvin Vest <calvinjvest@gmail.com>
2024-10-18 15:24:56 -0500

    Adds Problem0099.java with full functionality

--- src/projecteulersolutions/problems/Problem0099.java
+++ src/projecteulersolutions/problems/Problem0099.java
@@ -1,5 +1,7 @@
 package projecteulersolutions.problems;
 
+import java.io.*;
+
 /*
 Comparing two numbers written in index form like 2^11 and 3^7 is not difficult,
 as any calculator would confirm that 2^11 = 2048 < 3^7 = 2187.
@@ -21,6 +23,40 @@ public class Problem0099 extends Problem {
 
     @Override
     public void printSolution() {
-        System.out.println("This problem has not yet been solved.");
+        int maxLineNum = 0;
+        try {
+            maxLineNum = findMaxLineNum();
+        } catch (IOException e) {
+            System.out.println("Exception Encountered: problem0099.txt can not be found.");
+        }
+
+
+        System.out.println("The maximum value is found at problem0099.txt line #" + maxLineNum);
+    }
+
+    private int findMaxLineNum() throws IOException {
+        File problemFile = new File(Problem.FILEPATH + "problem0099.txt");
+        BufferedReader reader = new BufferedReader(new FileReader(problemFile));
+        String line;
+        int lineNumber = 0;
+        int maxLine = 0;
+        double maxLogValue = Double.NEGATIVE_INFINITY;
+
+        while ((line = reader.readLine()) != null) {
+            lineNumber++;
+            String[] strs = line.split(",");
+            double base = Double.parseDouble(strs[0]);
+            double exponent = Double.parseDouble(strs[1]);
+
+            double logValue = exponent * Math.log(base);
+
+            if (logValue > maxLogValue) {
+                maxLogValue = logValue;
+                maxLine = lineNumber;
+            }
+        }
+
+        reader.close();
+        return maxLine;
     }
 }
